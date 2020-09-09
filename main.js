4f0a00e0a11e2fd2058164a50f8770f015db63e9
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-09 19:52:46 -0400

    skipped 4 and 5 but completed 6

--- main.js
+++ main.js
@@ -6,5 +6,22 @@ console.log(sumOfPrices);
 const result = items.filter(item => item.price >= 14 && item.price <= 18);
 console.log(result);
 //my thought process is to start off with the filter method because I only want the items that match the criteria I am looking for
-const result2 = items.filter(item => item.currency_code === 'GBP');
-console.log(result2);
+const currency = items.filter(item => item.currency_code === 'GBP');
+console.log(currency);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const sellerMade = items.filter(item => item.who_made === 'i_did');
+console.log(sellerMade);
