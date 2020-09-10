656752524d589a09db0daa8b38d38fae10405e4e
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-10 09:57:58 -0400

    completed assignment

--- main.js
+++ main.js
@@ -1,23 +1,45 @@
 // my thought process is to start off with the reduce method because we want to reduce it to a single value. Which in this problem is price
+
 const sumOfPrices = items.reduce((sum, item) => sum + item.price, 0) / items.length;
 console.log(sumOfPrices);
+const avgPrice = `The average price is ${sumOfPrices}`
+console.log(avgPrice);
+  console.log('\n');
 
 //my thought process is to start off with the filter method because I only want the items that match the criteria I am looking for
-const result = items.filter(item => item.price >= 14 && item.price <= 18);
-console.log(result);
+const itemsBetween = `Items that cost between $14.00 USD and $18.00 USD:`
+console.log(itemsBetween);
+
+const result = items.filter(item => item.price >= 14 && item.price <= 18 && item.currency_code === 'USD');
+result.forEach(function(item) {
+  console.log(item.title);
+})
+  console.log('\n');
+
 //my thought process is to start off with the filter method because I only want the items that match the criteria I am looking for
-const currency = items.filter(item => item.currency_code === 'GBP');
-console.log(currency);
+const britItems = items.filter(item => item.currency_code === 'GBP')
+britItems.forEach(function(item) {
+  console.log(item.title);
+});
+  console.log('\n');
 
+const woodFind = items.filter(item => item.materials.includes('wood'))
+woodFind.forEach(function(item){
+  console.log(`${item.title} is made of wood.`);
+});
+  console.log('\n');
 
+const moreThanEight = items.filter(item => item.materials.length >= 8);
 
+moreThanEight.forEach(function(item) {
 
-
-
-
-const alotMat = items.filter(item => item.materials.length >= 8);
-console.log(alotMat);
+  console.log(`${item.title} has ${item.materials.length} materials.`);
+  item.materials.forEach(function(item){
+    console.log(item);
+  });
+  console.log('\n');
+});
 
 
 const sellerMade = items.filter(item => item.who_made === 'i_did');
-console.log(sellerMade);
+console.log(`${sellerMade.length} were made by their sellers`);
